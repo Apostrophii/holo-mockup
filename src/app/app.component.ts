52ee51c8a29414b38b54304bc145bc117fc64462
Luke Travis <luke.travis@k3integrations.com>
2017-03-16 19:57:43 -0700

    Initial working commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  @ViewChild('homescreen') homescreenElRef: ElementRef;
+  homescreenEl: HTMLDivElement;
+
+  ngOnInit() {
+    this.homescreenEl = this.homescreenElRef.nativeElement;
+
+    window.onresize = this.resizeHomescreen;
+    this.resizeHomescreen();
+  }
+
+  resizeHomescreen = () => {
+      const scale = Math.min(
+        window.innerWidth / this.homescreenEl.offsetWidth,
+        window.innerHeight / this.homescreenEl.offsetHeight
+      );
+      this.homescreenEl.style.transform = `translate(-50%, -50%) scale(${scale})`;
+    }
 }
